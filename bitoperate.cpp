e88f6ceec47957d7c2b51ce476851ebdabe8ffeb
thelxz <thelxz@163.com>
2018-06-09 22:20:12 +0800

    add 'from' and 'to' func for bit operate

--- bitoperate.cpp
+++ bitoperate.cpp
@@ -1,25 +1,33 @@
 #include "bitoperate.h"
-#include <QGridLayout>
+#include <QHBoxLayout>
 #include <QLineEdit>
 #include <QCheckBox>
 #include <QPushButton>
 #include <QWidget>
 #include <QRegExpValidator>
 #include <QComboBox>
+#include <QLabel>
+#include <algorithm>
+#include <QMessageBox>
 
 BitOperate::BitOperate(QWidget *parent, Bits *bits) : QWidget(parent)
 {
     this->bits = bits;
-    QGridLayout *mainLayout = new QGridLayout;
+    QHBoxLayout *mainLayout = new QHBoxLayout;
     QGridLayout *shiftLayout = new QGridLayout;
     QWidget * shiftWidget = new QWidget;
+    label_from = new QLabel("From");
+    label_to = new QLabel("To");
+    bit_range_from = new QLineEdit("0",this);
+    bit_range_to = new QLineEdit("63",this);
     btn_clear = new QPushButton("clear", this);
-    btn_set = new QPushButton("set all", this);
+    btn_set = new QPushButton("set", this);
     btn_reverse = new QPushButton("reverse", this);
     btn_shift_left = new QPushButton("<<shift",this);
     txt_shift_bit_num = new QLineEdit("0",this);
     btn_shift_right = new QPushButton("shift>>",this);
     shift_mode = new QComboBox(this);
+
     shift_mode->setToolTip("Shift Mode");
     shift_mode->addItem("Logic");
     shift_mode->addItem("Arith");
@@ -31,6 +39,18 @@ BitOperate::BitOperate(QWidget *parent, Bits *bits) : QWidget(parent)
     txt_shift_bit_num->setValidator(dec_validator);
     txt_shift_bit_num->setMaxLength(2);
     txt_shift_bit_num->setAlignment(Qt::AlignCenter);
+    bit_range_from->setValidator(dec_validator);
+    bit_range_from->setMaxLength(2);
+    bit_range_from->setAlignment(Qt::AlignCenter);
+
+    label_from->setFixedWidth(40);
+    label_to->setFixedWidth(20);
+    bit_range_from->setFixedWidth(40);
+    bit_range_to->setFixedWidth(40);
+
+    bit_range_to->setValidator(dec_validator);
+    bit_range_to->setMaxLength(2);
+    bit_range_to->setAlignment(Qt::AlignCenter);
 
     shiftLayout->addWidget(btn_shift_left,		0,0,1,1);
     shiftLayout->addWidget(txt_shift_bit_num,	0,1,2,1);
@@ -40,19 +60,25 @@ BitOperate::BitOperate(QWidget *parent, Bits *bits) : QWidget(parent)
     shiftWidget->setLayout(shiftLayout);
     shiftWidget->setFixedWidth(200);
 
-    mainLayout->addWidget(btn_clear,			0,0,1,1);
-    mainLayout->addWidget(btn_set,				0,1,1,1);
-    mainLayout->addWidget(btn_reverse,			0,2,1,1);
-    mainLayout->addWidget(shiftWidget,			0,3,1,1);
-    mainLayout->addWidget(shift_mode,			0,4,1,1);
+    mainLayout->addWidget(label_from);
+    mainLayout->addWidget(bit_range_from);
+    mainLayout->addWidget(label_to);
+    mainLayout->addWidget(bit_range_to);
+    mainLayout->addSpacing(10);
+    mainLayout->addWidget(btn_clear);
+    mainLayout->addWidget(btn_set);
+    mainLayout->addWidget(btn_reverse);
+    mainLayout->addSpacing(10);
+    mainLayout->addWidget(shiftWidget);
+    mainLayout->addWidget(shift_mode);
     this->setLayout(mainLayout);
     txt_shift_bit_num->setText("1");
-    btn_clear->setFocusPolicy(Qt::NoFocus);
-    btn_reverse->setFocusPolicy(Qt::NoFocus);
-    btn_set->setFocusPolicy(Qt::NoFocus);
-    btn_shift_left->setFocusPolicy(Qt::NoFocus);
-    btn_shift_right->setFocusPolicy(Qt::NoFocus);
-    shift_mode->setFocusPolicy(Qt::NoFocus);
+//    btn_clear->setFocusPolicy(Qt::NoFocus);
+//    btn_reverse->setFocusPolicy(Qt::NoFocus);
+//    btn_set->setFocusPolicy(Qt::NoFocus);
+//    btn_shift_left->setFocusPolicy(Qt::NoFocus);
+//    btn_shift_right->setFocusPolicy(Qt::NoFocus);
+//    shift_mode->setFocusPolicy(Qt::NoFocus);
     connect(shift_mode,SIGNAL(currentTextChanged(QString)),this,SLOT(set_shift_mode(QString)));
     connect(btn_clear,SIGNAL(clicked(bool)),this,SLOT(clear_num()));
     connect(btn_reverse,SIGNAL(clicked(bool)),this,SLOT(reverse_num()));
@@ -62,14 +88,62 @@ BitOperate::BitOperate(QWidget *parent, Bits *bits) : QWidget(parent)
     connect(bits,SIGNAL(value_changed()),this,SLOT(update_display()));
 }
 
+int BitOperate::get_range_start(){
+    int temp1, temp2;
+    int start,max_end;
+    temp1 = this->bit_range_from->text().toInt();
+    temp2 = this->bit_range_to->text().toInt();
+    max_end = (int)bits->get_width()-1;
+    if (temp1>max_end && temp2 > max_end)
+        start = -1;
+    else
+        start = std::min(temp1,temp2);
+    return start;
+}
+
+int BitOperate::get_range_end(){
+    int temp1, temp2;
+    int end,max_end;
+    temp1 = this->bit_range_from->text().toInt();
+    temp2 = this->bit_range_to->text().toInt();
+    max_end = (int)bits->get_width()-1;
+    if (temp1>max_end && temp2 > max_end)
+        end = -1;
+    else
+        end = std::max(temp1,temp2);
+    return std::min(end,(int)bits->get_width()-1);
+
+}
+
 void BitOperate::clear_num(){
-    bits->clear_all();
+    if ((get_range_end()==-1)||(get_range_start()==-1)){
+        QMessageBox msgBox;
+        msgBox.setText("Please check the range!");
+        msgBox.exec();
+        return;
+    }else{
+        bits->clear_bits(get_range_start(),get_range_end());
+    }
 }
 void BitOperate::set_num(){
-    bits->set_all();
+    if ((get_range_end()==-1)||(get_range_start()==-1)){
+        QMessageBox msgBox;
+        msgBox.setText("Please check the range!");
+        msgBox.exec();
+        return;
+    }else{
+        bits->set_bits(get_range_start(),get_range_end());
+    }
 }
 void BitOperate::reverse_num(){
-    bits->reverse_all();
+    if ((get_range_end()==-1)||(get_range_start()==-1)){
+        QMessageBox msgBox;
+        msgBox.setText("Please check the range!");
+        msgBox.exec();
+        return;
+    }else{
+        bits->reverse_bits(get_range_start(),get_range_end());
+    }
 }
 
 void BitOperate::shift_left(){
--- bitoperate.h
+++ bitoperate.h
@@ -6,6 +6,7 @@ class QPushButton;
 class QCheckBox;
 class QLineEdit;
 class QComboBox;
+class QLabel;
 #include "bits.h"
 
 class BitOperate : public QWidget
@@ -28,8 +29,15 @@ private:
     QPushButton * btn_set;
     QPushButton * btn_shift_left;
     QLineEdit 	* txt_shift_bit_num;
+    QLabel		* label_from;
+    QLabel		* label_to;
+    QLineEdit 	* bit_range_from;
+    QLineEdit 	* bit_range_to;
     QPushButton * btn_shift_right;
     QComboBox * shift_mode;
+int get_range_start();
+int get_range_end();
+
 private slots:
     void update_display();
     void set_shift_mode(QString mode);
